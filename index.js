a96eb81d033c95aeea9383c6792da21c796211dc
Odemi Tosanwumi <odemi24@my.yorku.ca>
2022-03-12 18:11:04 -0500

    setTimeout added function to buttonAnimation function to remove pressed class. Removes class after .1 second.

--- index.js
+++ index.js
@@ -79,4 +79,8 @@ function buttonAnimation (currentKey) {
     var activeButton = document.querySelector("." + currentKey);
 
     activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
